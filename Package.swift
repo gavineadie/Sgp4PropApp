afcd441de9e96184b3c495bb14056dbb5fcc65cf
Gavin Eadie <gavin@umich.edu>
2025-03-19 23:23:55 -0400

    update "Sgp4PropLib" version to 9.0+ ..

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,7 @@ let package = Package(
         .macOS(.v11)                    // minimum macOS version v11 (Big Sur)
     ],
     dependencies: [
-        .package(url: "https://github.com/gavineadie/Sgp4PropLib.git", from: "0.0.1"),
+        .package(url: "https://github.com/gavineadie/Sgp4PropLib.git", from: "9.0.0"),
 //      .package(name: "Sgp4PropLib", path: "../Sgp4PropLib")
     ],
     targets: [
